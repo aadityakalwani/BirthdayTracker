02620ad33e29b8645d0bdcd1562a84d13235fde6
aadityakalwani <aadityakalwani@gmail.com>
2021-08-08 10:03:10 +0100

    made my own loop  now it can fetch, add, and store birthdays  the only issue is that the dict is forgotten every time i close the file  gonna have to workout how to fix this

--- main.py
+++ main.py
@@ -2,20 +2,35 @@
 # may the coding begin
 
 birthdays = {
-    "Bhavya": "Jan 1st"
+    "Bhavya": "January 1st, 2006"
             }
 
 
 while True:
-    print("Enter a name: (blank to quit)")
-    name = input()
-    if name == '':
-    break
-if name in birthdays:
-    print(birthdays[name] + ' is the birthday of ' + name)
-else:
-    print('I do not have birthday information for ' + name)
-    print('What is their birthday?')
-    bday = input()
-    birthdays[name] = bday
-    print('Birthday database updated.')
+    name = input("At any time, you can press q to quit.\nEnter a name: ")
+
+    if name.lower == "q":
+        break
+
+    if name in birthdays:
+        print(f"{birthdays[name]} is the birthday of {name}")
+
+    else:
+        print(f"{name} is not in this dictionary.")
+        bday = input("Enter their birthday: ")
+        birthdays[name] = bday
+        print("Birthdays database and dictionary updated.")
+
+# while True:
+#     print("Enter a name: (blank to quit)")
+#     name = input()
+#     if name == '':
+#         break
+# if name in birthdays:
+#     print(birthdays[name] + ' is the birthday of ' + name)
+# else:
+#     print('I do not have birthday information for ' + name)
+#     print('What is their birthday?')
+#     bday = input()
+#     birthdays[name] = bday
+#     print('Birthday database updated.')
