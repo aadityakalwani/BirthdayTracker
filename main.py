16a9b70de37988457a26afe0cfeb1b3984910219
aadityakalwani <aadityakalwani@gmail.com>
2021-08-10 12:21:57 +0100

    tried to import and sue numpy to save the dict, but turns out that's a bad idea  im going to use pickle instead

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 # ciao
 # may the coding begin
 
+import numpy as np
+
 birthdays = {
     "Bhavya": "January 1st, 2006"
             }
 
 
 while True:
+
     name = input("At any time, you can press q to quit.\nEnter a name: ")
 
     if name.lower == "q":
         break
+
+    # Load
+    read_dictionary = np.load("main.py", allow_pickle=True).item()
+    print(read_dictionary[name])  # displays "bday"
 
     if name in birthdays:
         print(f"{birthdays[name]} is the birthday of {name}")
@@ -20,17 +27,15 @@ while True:
         bday = input("Enter their birthday: ")
         birthdays[name] = bday
         print("Birthdays database and dictionary updated.")
+        # save dict:
+        np.save("main.py", birthdays)
 
-# while True:
-#     print("Enter a name: (blank to quit)")
-#     name = input()
-#     if name == '':
-#         break
-# if name in birthdays:
-#     print(birthdays[name] + ' is the birthday of ' + name)
-# else:
-#     print('I do not have birthday information for ' + name)
-#     print('What is their birthday?')
-#     bday = input()
-#     birthdays[name] = bday
-#     print('Birthday database updated.')
+# import numpy as np
+#
+# # Save
+# dictionary = {'hello':'world'}
+# np.save('my_file.npy', dictionary)
+#
+# # Load
+# read_dictionary = np.load('my_file.npy',allow_pickle='TRUE').item()
+# print(read_dictionary['hello']) # displays "world"
