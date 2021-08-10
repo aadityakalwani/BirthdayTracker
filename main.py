be19814a386fb4473fcfe0d63598385f6901bd89
aadityakalwani <aadityakalwani@gmail.com>
2021-08-10 12:23:56 +0100

    back to base lol  so uh now the task is to use pickle to save this dict and update the information within it

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # ciao
 # may the coding begin
 
-import numpy as np
+
 
 birthdays = {
     "Bhavya": "January 1st, 2006"
@@ -15,10 +15,6 @@ while True:
     if name.lower == "q":
         break
 
-    # Load
-    read_dictionary = np.load("main.py", allow_pickle=True).item()
-    print(read_dictionary[name])  # displays "bday"
-
     if name in birthdays:
         print(f"{birthdays[name]} is the birthday of {name}")
 
@@ -27,15 +23,3 @@ while True:
         bday = input("Enter their birthday: ")
         birthdays[name] = bday
         print("Birthdays database and dictionary updated.")
-        # save dict:
-        np.save("main.py", birthdays)
-
-# import numpy as np
-#
-# # Save
-# dictionary = {'hello':'world'}
-# np.save('my_file.npy', dictionary)
-#
-# # Load
-# read_dictionary = np.load('my_file.npy',allow_pickle='TRUE').item()
-# print(read_dictionary['hello']) # displays "world"
