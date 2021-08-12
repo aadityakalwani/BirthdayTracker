76182b130745695d76765dc140e0123b30144985
aadityakalwani <aadityakalwani@gmail.com>
2021-08-12 23:20:25 +0100

    improved code to allow to search by name, now i need to be abel to search by date

--- main.py
+++ main.py
@@ -6,33 +6,37 @@ import json
 with open("birthdays.json") as f:
     birthdays = json.load(f)
 
-while True:
+    begin_loop: str = input("At any time you can press q to quit.\n"
+                            "type 'date' to search by date\n"
+                            "type 'name' to search by name\n"
+                            "Enter your command here: ").lower()
 
-    name: str = input("At any time you can press q to quit.\nEnter a name: ").lower()
+    if begin_loop.lower() == "q":
+        print("You have quit.")
 
-    if name.lower() == "q":
-        print("You have quit this loop.")
-        break
+    if begin_loop.lower() == "name":
+        while True:
+            name: str = input("This will search the dict for a person.\nEnter their name here: ")
+            if name in birthdays:
+                print(f"{name}'s birthday is on {birthdays[name]}")
 
-    if name in birthdays:
-        print(f"{name}'s birthday is on {birthdays[name]}")
+            else:
+                new_birthday = input(f"{name} is not in this dictionary.\nEnter their birthday: ")
+                birthdays.setdefault(name, new_birthday)
 
-    else:
-        new_birthday = input(f"{name} is not in this dictionary.\nEnter their birthday: ")
-        birthdays.setdefault(name, new_birthday)
+                # this is all the code that i used to run in this else statement
+                # now its a lot shorter using the set and get
 
-        # this is all the code that i used to run in this else statement, now its a lot shorter using the set and get
-
-        # bday = input("Enter their birthday: ")
-        #
-        # if bday.lower() == "q":
-        #     print("You have quit this loop.")
-        #     break
-        #
-        # birthdays[name] = bday
-        # with open("birthdays.json", "w") as f:
-        #     json.dump(birthdays, f, indent=2)
-        # print("Birthdays database and dictionary updated.")
+                # bday = input("Enter their birthday: ")
+                #
+                # if bday.lower() == "q":
+                #     print("You have quit this loop.")
+                #     break
+                #
+                # birthdays[name] = bday
+                # with open("birthdays.json", "w") as f:
+                #     json.dump(birthdays, f, indent=2)
+                # print("Birthdays database and dictionary updated.")
 
 # how to order/sort the dict:
 # the key here is not to use a dictionary
